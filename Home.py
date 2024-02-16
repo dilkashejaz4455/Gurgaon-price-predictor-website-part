3449ebc604d533cc3f1f2ed21619f5f0e85d517a
Dilkash Ejaz <123047398+dilkashejaz4455@users.noreply.github.com>
2024-02-16 18:52:28 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+
+
+st.sidebar.success("Select a demo above.")
+
+st.title("Welcome to Gurugram Property Predictor")
+
+st.markdown("**Unlock the Power of Data to Predict Property Prices**")
+
+st.markdown("Are you curious about the real estate market in Gurugram? Look no further! Our innovative application combines cutting-edge technology with comprehensive data analysis to provide accurate predictions for Gurgaon's properties, be it apartments or houses.")
+
+st.header("Features:")
+st.markdown("1. **Property Price Prediction:** Wondering about the future value of a property in Gurugram? Our advanced algorithms crunch the numbers to give you reliable price predictions, helping you make informed decisions.")
+st.markdown("2. **Analytics Section:** Dive into insightful maps and graphs that depict trends and patterns in Gurugram's real estate market. Stay ahead of the curve with our in-depth analytics.")
+st.markdown("3. **Apartment Predictor:** Looking to buy or rent an apartment? Our apartment predictor tool uses machine learning to suggest the most suitable options based on your preferences and budget.")
+
+st.header("Why Choose Us?")
+st.markdown("- **Accuracy:** Our predictions are backed by robust data analysis and machine learning models, ensuring high accuracy.")
+st.markdown("- **User-Friendly Interface:** Our application is designed with simplicity in mind, making it easy for anyone to navigate and use.")
+st.markdown("- **Comprehensive Insights:** Gain a deeper understanding of Gurugram's property market with our comprehensive analytics and predictive tools.")
+
+st.header("Get Started Today")
+
+
+    
+
+
+
